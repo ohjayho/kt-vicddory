7c5151dd9dfab3e876846c74171abdb70d5c43b0
SNXWXH <snxwxh@gmail.com>
2024-06-27 14:35:50 +0900

    🚚 Chore: 프로젝트 파일 폴더 구조

---
+++ src/app/(test)/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestMain() {
+  return (
+    <>
+      <h1>TestMain Component</h1>
+    </>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>KTwiz 메인</h1>
-      <p>디스코드 연동 확인</p>
-      <p>충돌 예시</p>
-    </>
-  );
-}
