6f218b5934f10e29c44ef2f011a5cf8f3a930cd7
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-01 01:51:18 +0900

    🔧 Modify: 뉴스 컴포넌트 생성
    뉴스 컴포넌트 생성과 디자인.
    props로 방향과 뉴스 내용을 전달받음.
    전달받은 방향은 각 뉴스 버블의 모서리의 radius를 결정함.
    최상단 div의 가상 선택자는 뉴스 버블
    간 수직 간격을 줄이기 위해 사용.

---
+++ src/components/wiznews/NewsBalloon.tsx
@@ -0,0 +1,28 @@
+export default function NewsBalloon({
+  direction,
+  content,
+}: {
+  direction: 'left' | 'right';
+  content: string;
+}) {
+  let position = direction === 'right' ? 'justify-end' : 'justify-start';
+  let setDirection =
+    direction === 'right' ? 'rounded-ee-none' : 'rounded-es-none';
+  return (
+    <>
+      <div className={`flex ${position} [&:not(:first-child)]:-mt-10`}>
+        <div
+          className={`w-[400px] h-[100px] rounded-[54px] ${setDirection} bg-gradient-to-br from-[#FF0000] via-[#620000] to-[#FF0000] p-2`}
+        >
+          <div
+            className={`w-full h-full rounded-[46px] ${setDirection} bg-white flex justify-center items-center shadow-[inset_0px_6px_4px_rgba(0,0,0,0.25)]`}
+          >
+            <h1 className="flex items-center w-11/12 h-5/6 font-['DungGeunMo'] overflow-hidden overflow-ellipsis break-all ">
+              {content}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
