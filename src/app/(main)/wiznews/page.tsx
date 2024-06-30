166cc77aa7d846217ff1fcf53e86d51b527addac
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-01 01:41:51 +0900

    💄 Design: 뉴스 페이지 디자인
    간단히 뉴스 페이지 레이아웃 설계

--- src/app/(main)/wiznews/page.tsx
+++ src/app/(main)/wiznews/page.tsx
@@ -1,7 +1,17 @@
 export default function WizNews() {
   return (
     <>
-      <h1>WizNews Component</h1>
+      <div className="min-h-screen bg-black">
+        <header className="h-[83px] bg-pink-200"></header>
+        <div className="h-[253px] bg-[url('/images/wiznews/wiznews_banner.png')] bg-cover bg-center flex flex-col justify-center items-center text-white">
+          <h1>wiz 한줄 뉴스</h1>
+          <h1>kt wiz의 새소식을 빅 또리 기자가 쉽고 빠르게 전해드립니다.</h1>
+        </div>
+        <div className="w-full h-[800px] flex justify-center  bg-[url('/images/wiznews/wiznews_bg.png')] bg-cover bg-center">
+          <div className="absolute bottom-4 left-0"></div>
+          <section className="w-[820px] flex flex-col"></section>
+        </div>
+      </div>
     </>
   );
 }
