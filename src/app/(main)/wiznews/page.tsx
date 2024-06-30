bf7b1f9eaed9b6a407e6475e1cfd43c39857ce7a
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-01 01:56:25 +0900

    💄 Design: 뉴스 컴포넌트 테스트
    뉴스 페이지 내부에서 뉴스 컴포넌트 렌더링 상태 확인

--- src/app/(main)/wiznews/page.tsx
+++ src/app/(main)/wiznews/page.tsx
@@ -1,3 +1,5 @@
+import NewsBalloon from '@/components/wiznews/NewsBalloon';
+
 export default function WizNews() {
   return (
     <>
@@ -9,7 +11,16 @@ export default function WizNews() {
         </div>
         <div className="w-full h-[800px] flex justify-center  bg-[url('/images/wiznews/wiznews_bg.png')] bg-cover bg-center">
           <div className="absolute bottom-4 left-0"></div>
-          <section className="w-[820px] flex flex-col"></section>
+          <section className="w-[820px] flex flex-col">
+            <NewsBalloon
+              direction="right"
+              content="KT 위즈는 6월 19일 롯데전에서 에픽하이를 초청해 시구 행사를
+            진행합니다 s5ad4f65dsa4fsa89vc56zxv89cx4sadsadf8d9zsf4dsz89f4ds89f4."
+            />
+            <NewsBalloon direction="left" content="나는 오재호다." />
+            <NewsBalloon direction="right" content="나는 빅이다." />
+            <NewsBalloon direction="left" content="나는 또리다." />
+          </section>
         </div>
       </div>
     </>
