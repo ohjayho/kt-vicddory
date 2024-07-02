238c12ca6fbb604517e3f5b6dac2ed5287d2d5b9
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-02 11:19:01 +0900

    💄 Design: 나눔스퀘어 네오 폰트 추가 후 body에 적용
    나눔스퀘어 네오 폰트 추가 후 최상단 layout.tsx의 body에 폰트 적용.
    font-face에 여러 줄이 있는 이유는 각 브라우저에 맞는 확장자를 제공하기
    위해서임.
    format 속성을 사용해 브라우저에서 지원 가능한 파일만 다운로드함.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="font-['NanumSquareNeo']">{children}</body>
     </html>
   );
 }
