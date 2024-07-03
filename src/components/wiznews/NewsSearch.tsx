939a50d7a0bb86cd3304ea2c794e385341864b74
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-03 17:21:04 +0900

    💄 Design: 뉴스 검색 컴포넌트 디자인 수정
    컴포넌트 위 아래 margin, input 내부 shadow 추가와 raidus 수정

--- src/components/wiznews/NewsSearch.tsx
+++ src/components/wiznews/NewsSearch.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 export default function NewsSearch() {
   return (
     <>
-      <div className="flex justify-between w-[480px]">
+      <div className="flex justify-between w-[480px] my-7">
         <Image src="/svgs/emblem.svg" alt="KT emblem" width={35} height={30} />
         <input
           type="text"
           name=""
           id=""
-          className="font-['DungGeunMo'] w-[436px] px-3 rounded-xl"
+          className="font-['DungGeunMo'] w-[436px] px-3 rounded-[10px] shadow-[inset_2px_3px_4px_rgba(0,0,0,0.25)]"
           placeholder="KT 위즈, 김민 선수, 수원 케이티 위즈 파크"
         />
       </div>
