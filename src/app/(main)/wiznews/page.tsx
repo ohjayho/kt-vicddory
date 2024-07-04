092fcc7df9ba79daddef50350cf1cd5c8532e987
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-04 10:28:30 +0900

    🔧 Modify: 뉴스 페이지 전용 레이아웃 생성

--- src/app/(main)/wiznews/page.tsx
+++ src/app/(main)/wiznews/page.tsx
@@ -1,38 +1,33 @@
 import Mascot from '@/components/wiznews/Mascot';
 import NewsBalloon from '@/components/wiznews/NewsBalloon';
-import NewsBanner from '@/components/wiznews/NewsBanner';
 import NewsSearch from '@/components/wiznews/NewsSearch';
 
 export default function WizNews() {
   return (
     <>
-      <div className="min-h-screen bg-black">
-        <header className="h-[83px] bg-pink-200 sticky top-0"></header>
-        <NewsBanner />
-        <div className="w-full flex justify-center  bg-[url('/images/mainBg.png')] bg-cover relative overflow-hidden">
-          <Mascot src="svgs/newsMascot/ddory.svg" location="left" />
-          <Mascot src="svgs/newsMascot/vic.svg" location="right" />
-          <section className="w-3/4 flex flex-col justify-center items-center z-10">
-            <NewsSearch />
-            <div className="w-[820px] h-[600px] overflow-y-scroll no-scrollbar">
-              <NewsBalloon
-                direction="right"
-                content="KT 위즈는 6월 19일 롯데전에서 에픽하이를 초청해 시구 행사를
+      <div className="w-full flex justify-center  bg-[url('/images/mainBg.png')] bg-cover relative overflow-hidden">
+        <Mascot src="svgs/newsMascot/ddory.svg" location="left" />
+        <Mascot src="svgs/newsMascot/vic.svg" location="right" />
+        <section className="w-3/4 flex flex-col justify-center items-center z-10">
+          <NewsSearch />
+          <div className="w-[820px] h-[600px] overflow-y-scroll no-scrollbar">
+            <NewsBalloon
+              direction="right"
+              content="KT 위즈는 6월 19일 롯데전에서 에픽하이를 초청해 시구 행사를
             진행합니다 s5ad4f65dsa4fsa89vc56zxv89cx4sadsadf8d9zsf4dsz89f4ds89f4."
-              />
-              <NewsBalloon direction="left" content="나는 오재호다." />
-              <NewsBalloon direction="right" content="나는 빅이다." />
-              <NewsBalloon direction="left" content="나는 또리다." />
-              <NewsBalloon direction="right" content="나는 빅이다." />
-              <NewsBalloon direction="left" content="나는 또리다." />
-              <NewsBalloon direction="right" content="나는 빅이다." />
-              <NewsBalloon direction="left" content="나는 또리다." />
-              <NewsBalloon direction="right" content="나는 빅이다." />
-              <NewsBalloon direction="left" content="나는 또리다." />
-              <NewsBalloon direction="right" content="나는 빅이다." />
-            </div>
-          </section>
-        </div>
+            />
+            <NewsBalloon direction="left" content="나는 오재호다." />
+            <NewsBalloon direction="right" content="나는 빅이다." />
+            <NewsBalloon direction="left" content="나는 또리다." />
+            <NewsBalloon direction="right" content="나는 빅이다." />
+            <NewsBalloon direction="left" content="나는 또리다." />
+            <NewsBalloon direction="right" content="나는 빅이다." />
+            <NewsBalloon direction="left" content="나는 또리다." />
+            <NewsBalloon direction="right" content="나는 빅이다." />
+            <NewsBalloon direction="left" content="나는 또리다." />
+            <NewsBalloon direction="right" content="나는 빅이다." />
+          </div>
+        </section>
       </div>
     </>
   );
