bd73a160d5aa75a5482eb1ab58333c054fafd32c
yheseul <kangyheseul@naver.com>
2024-07-06 17:15:49 +0900

    💄 Design: 디자인 변경

--- src/components/test/Balloon.tsx
+++ src/components/test/Balloon.tsx
@@ -1,17 +1,18 @@
 import { ReactNode } from "react";
 
-export default function Balloon({ children }: { children: ReactNode }) {
+type TBalloonProps = {
+  children: ReactNode;
+  className: string;
+};
+
+export default function Balloon({ children, className }: TBalloonProps) {
   return (
-    <div className="relative m-20 p-2 bg-white border-2 border-blue-200 rounded-full w-96 h-24">
-      <div
-        className="absolute w-0 h-0 border-x-[12px] border-x-transparent border-b-[24px] border-b-white"
-        style={{ top: '-21px', left: '192px', zIndex: 1 }}
-      ></div>
-      <div
-        className="absolute w-0 h-0 border-x-[12px] border-x-transparent border-b-[24px] border-b-blue-200"
-        style={{ top: '-23px', left: '192px', zIndex: 0 }}
-      ></div>
-      {children}
-    </div>
+    <>
+      <div className={className}>
+        <div className="relative m-20 p-2 bg-white border-4 border-blue-200 rounded-full w-96 h-16 text-[#333333] flex justify-center items-center">
+          {children}
+        </div>
+      </div>
+    </>
   );
 }
