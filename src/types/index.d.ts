9b9b575c87c4494daf7b5555c469c6201278989b
yheseul <kangyheseul@naver.com>
2024-07-06 22:00:48 +0900

    🔧 Modify: 타입 모듈화

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,11 @@
-//code base
+type TResultPositionProps = {
+  src: string;
+  alt: string;
+  width: number;
+  left: number;
+  text?: string;
+  description?: string;
+  tags?: string;
+};
+
+export { TResultPositionProps };
