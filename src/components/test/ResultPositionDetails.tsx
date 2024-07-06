55910a56946ca7f0df3e30c15a8f37a61cf652b0
yheseul <kangyheseul@naver.com>
2024-07-06 21:54:55 +0900

    🔧 Modify: 테스트 결과 페이지에서 포지션 내용 컴포넌트 생성

---
+++ src/components/test/ResultPositionDetails.tsx
@@ -0,0 +1,14 @@
+type TResultPositionDetailsProps ={
+  description: string;
+  tags: string;
+}
+
+export default function ResultPositionDetails ({ description, tags }: TResultPositionDetailsProps) {
+
+  return (
+    <>
+      <div className="text-[#333333] text-2xl font-bold mt-12">{description}</div>
+      <div className="text-[#BD1C1F] font-bold mt-2">{tags}</div>
+    </>
+  )
+}
