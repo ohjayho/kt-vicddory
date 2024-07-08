e26219657a7a1bf8a266ac4c63b9f6c918d8a364
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-08 23:55:43 +0900

    💄 Design: 뉴스 배너 반응형 추가
    작은 모바일 기기의 경우 줄바꿈.

--- src/components/wiznews/NewsBanner.tsx
+++ src/components/wiznews/NewsBanner.tsx
@@ -4,8 +4,9 @@ export default function NewsBanner() {
       <div className="pt-20">
         <div className="h-[252px] bg-[url('/images/wiznews/newsBanner.png')] bg-cover bg-center flex flex-col justify-center items-center text-white">
           <h1 className="text-4xl">wiz 한줄 뉴스</h1>
-          <h1 className="mt-6">
-            kt wiz의 새소식을 빅 또리 기자가 쉽고 빠르게 전해드립니다.
+          <h1 className="mt-6 text-center">
+            kt wiz의 새소식을 빅 또리 기자가
+            <br className="sm:hidden" /> 쉽고 빠르게 전해드립니다.
           </h1>
         </div>
       </div>
