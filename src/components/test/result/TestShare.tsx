a0240cea8a615ef973c8fb4569120850d591c674
yheseul <kangyheseul@naver.com>
2024-07-08 20:09:53 +0900

    🔧 Modify: 테스트 공유 버튼 ui 컴포넌트 생성

---
+++ src/components/test/result/TestShare.tsx
@@ -0,0 +1,19 @@
+import KakaoShare from "./KakaoShare";
+import { PiPaperclipLight } from "react-icons/pi";
+import { CiCamera } from "react-icons/ci";
+
+export default function TestShare () {
+  return (
+    <>
+      <div className='flex flex-row justify-center items-center gap-6 mt-3'>
+        <KakaoShare />
+        <button className='flex justify-center items-center border border-black rounded-full w-14 h-14 text-4xl bg-white hover:bg-gray-100 hover:border-0'>
+          <PiPaperclipLight />
+        </button>
+        <button className='flex justify-center items-center border border-black rounded-full w-14 h-14 text-4xl bg-white hover:bg-gray-100 hover:border-0'>
+          <CiCamera />
+        </button>        
+      </div>
+    </>
+  )
+}
