729ebbf24c5ec8cd69dfc1cda289f39bf23558bb
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-08 23:55:04 +0900

    💄 Design: 뉴스 말풍선 반응형 추가

--- src/components/wiznews/NewsBalloon.tsx
+++ src/components/wiznews/NewsBalloon.tsx
@@ -12,10 +12,12 @@ export default function NewsBalloon({
     direction === 'right' ? 'rounded-ee-none' : 'rounded-es-none';
   return (
     <>
-      <div className={`flex ${position} [&:not(:first-child)]:-mt-10`}>
+      <div
+        className={`flex ${position} max-lg:justify-center lg:[&:not(:first-child)]:-mt-10 max-lg:mb-5`}
+      >
         <Link href="/wiznews/a" scroll={false}>
           <div
-            className={`w-[400px] h-[120px] rounded-[54px] ${setDirection} bg-gradient-to-br from-[#FF0000] via-[#620000] to-[#FF0000] p-2`}
+            className={`w-[400px] max-lg:w-[380px] h-[120px] rounded-[54px] ${setDirection} bg-gradient-to-br from-[#FF0000] via-[#620000] to-[#FF0000] p-2`}
           >
             <div
               className={`w-full h-full rounded-[46px] ${setDirection} bg-white flex justify-center items-center shadow-[inset_0px_6px_4px_rgba(0,0,0,0.25)]`}
