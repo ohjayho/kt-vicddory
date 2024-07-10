47d6cb2aab75cd39a0dd613ca2273798d734d0b2
yheseul <kangyheseul@naver.com>
2024-07-10 11:37:37 +0900

    ✨ Feat: 캡쳐 버튼에 이벤트 핸들러 전달 받음

--- src/components/test/result/TestShare.tsx
+++ src/components/test/result/TestShare.tsx
@@ -1,9 +1,15 @@
 import KakaoShare from "./KakaoShare";
 import { PiPaperclipLight } from "react-icons/pi";
-import { CiCamera } from "react-icons/ci";
 import UrlCopy from "./UrlCopy";
+import React from "react";
+import { CiCamera } from "react-icons/ci";
 
-export default function TestShare () {
+type TTestShareProps = {
+	onClick(): void;
+}
+
+export default function TestShare({ onClick }: TTestShareProps) {
+
   return (
     <>
       <div className='flex flex-row justify-center items-center gap-6 mt-3'>
@@ -11,9 +17,9 @@ export default function TestShare () {
         <button onClick={UrlCopy} className='flex justify-center items-center border border-black rounded-full w-14 h-14 text-4xl bg-white hover:bg-gray-100 hover:border-0'>
           <PiPaperclipLight />
         </button>
-        <button className='flex justify-center items-center border border-black rounded-full w-14 h-14 text-4xl bg-white hover:bg-gray-100 hover:border-0'>
-          <CiCamera />
-        </button>        
+        <button onClick={onClick} className='flex justify-center items-center border border-black rounded-full w-14 h-14 text-4xl bg-white hover:bg-gray-100 hover:border-0'>
+        <CiCamera />
+      </button>
       </div>
     </>
   )
