e757865ffce1a04cf8b6940b426b0e1ef87cf926
mj <mjk4534@ewhain.net>
2024-07-13 18:25:46 +0900

    💄 Design: 카드 앞면 상세페이지 큰 버전 CSS 변경

--- src/components/tradingCard/CardFront.tsx
+++ src/components/tradingCard/CardFront.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-
+import PlayerCardProps from '@/components/tradingCard/PlayerCard';
 interface IPitcher {
   korName: string;
   engName: string;
@@ -15,11 +15,45 @@ interface CardFrontProps {
 const sizeClasses = {
   small: 'w-48 h-72', // character test
   medium: 'w-[252px] h-[348px]', // list
-  large: 'w-[400px] h-[560px]', // detail page
+  large: 'w-[360px] h-[500px]', // detail page
 };
 
+const photoSizes = {
+  small: 'w-40 h-60',
+  medium: 'w-[228px] h-[316px]',
+  large: 'w-[326px] h-[454px]',
+};
+
+const textBoxes = {
+  small: 'right-1 bottom-0',
+  medium: 'right-3 bottom-0',
+  large: 'right-6 bottom-5',
+};
+
+const numberFonts = {
+  small: 'text-sm',
+  medium: 'text-base',
+  large: 'text-xl',
+};
+
+const nameBoxes = {
+  small: 'pt-0 pb-0',
+  medium: 'pt-1.5 pb-1 pr-4',
+  large: 'pt-4 pr-6',
+};
+
+const korNameFonts = {
+  small: 'text-lg',
+  medium: 'text-xl',
+  large: 'text-3xl',
+};
 const CardFront: React.FC<CardFrontProps> = ({ player, size = 'medium' }) => {
   const sizeClass = sizeClasses[size];
+  const photoSize = photoSizes[size];
+  const textBox = textBoxes[size];
+  const numberFont = numberFonts[size];
+  const nameBox = nameBoxes[size];
+  const korNameFont = korNameFonts[size];
   // 카드 위에 마우스 올리면 카드가 회전하는 코드
   const cardRef = useRef<HTMLDivElement>(null);
 
@@ -48,56 +82,56 @@ const CardFront: React.FC<CardFrontProps> = ({ player, size = 'medium' }) => {
     };
   }, []);
 
-  if (size === 'medium') {
-    return (
-      <>
-        <div>
-          {/* 홀로그램 이미지 */}
-          <div
-            className={`absolute ${sizeClass} inset-x bg-hologram-gradient brightness-110 opacity-80 mix-blend-color-dodge bg-cover bg-[150%_150%] transition-all duration-100`}
-            ref={cardRef}
-          >
-            {' '}
+  return (
+    <>
+      <div>
+        {/* 홀로그램 이미지 */}
+        <div
+          className={`absolute ${sizeClass} inset-x bg-hologram-gradient brightness-110 opacity-80 mix-blend-color-dodge bg-cover bg-[150%_150%] transition-all duration-100`}
+          ref={cardRef}
+        ></div>
+        {/* 카드 앞면 */}
+        <div
+          className={`bg-black ${sizeClass} rounded-lg shadow-black/40 overflow-visible relative m-4 shadow-lg items-center justify-center transform transition-transform duration-100`}
+        >
+          {/* 선수 이미지 */}
+          <div className={`relative ${photoSize} mx-auto`}>
+            <img
+              src={`/images/player/playerCardFront/${player.playerImg}`}
+              alt={`${player.korName} Image`}
+              className="absolute left-0 top-3 w-full h-full object-cover rounded-2xl"
+            />
           </div>
-          {/* 카드 앞면 */}
+          {/*프레임 이미지*/}
+          <div className="absolute inset-0">
+            <img
+              src={'/images/frontCardFrame.png'}
+              alt={'Front Card Frame'}
+              className="w-full h-full object-cover"
+            />
+          </div>
           <div
-            className={`bg-black rounded-lg shadow-black/40 overflow-visible relative m-4 shadow-lg items-center justify-center transform transition-transform duration-100 ${sizeClass}`}
+            className={`absolute inset-0 flex flex-col justify-end p-6 text-white text-sm font-extrabold ${textBox}`}
           >
-            {/* 선수 이미지 */}
-            <div className="relative w-[228px] h-[316px] mx-auto">
-              <img
-                src={`/images/player/playerCardFront/${player.playerImg}`}
-                alt={`${player.korName} Image`}
-                className="absolute left-0 top-3 w-full h-full object-cover rounded-2xl"
-              />
-            </div>
-            {/*프레임 이미지*/}
-            <div className="absolute inset-0">
-              <img
-                src={'/images/frontCardFrame.png'}
-                alt={'Front Card Frame'}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="absolute inset-0 flex flex-col justify-end p-6 text-white text-sm font-extrabold right-3 bottom-0">
-              {/* 선수 번호 */}
-              <div className="leading-5 text-right text-lg ">{`No. ${player.backNum}`}</div>
-              <div className="flex justify-end items-center space-x-4 pt-1 pr-5">
-                {/* 선수 국문 이름 */}
-                <div className="text-2xl leading-relaxed text-right pl-8 pt-1">
-                  {player.korName}
-                </div>
-                {/* 선수 영문 이름
-              <div className="font-bold pt-3">{player.engName}</div> */}
+            {/* 선수 번호 */}
+            <div
+              className={`leading-5 text-right ${numberFont}`}
+            >{`No. ${player.backNum}`}</div>
+            <div
+              className={`flex justify-end items-center space-x-4 ${nameBox}`}
+            >
+              {/* 선수 국문 이름 */}
+              <div
+                className={`text-3xl text-right flex items-center ${korNameFont}`}
+              >
+                {player.korName}
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
-  if (size === 'large') {
-  }
+      </div>
+    </>
+  );
 };
 
 export default CardFront;
