60090ffb0013fc09236bd7f8930a4ab9510d8b59
mj <mjk4534@ewhain.net>
2024-07-13 19:26:56 +0900

    🔥 WIP: 선수기록 상세보기 추가

--- src/app/(main)/player/pitcher/[picherId]/page.tsx
+++ src/app/(main)/player/pitcher/[picherId]/page.tsx
@@ -22,7 +22,7 @@ const pitcherData = [
   },
 ];
 
-export default function page() {
+export default function PitcherDetail() {
   const PlayerChart = dynamic(() => import('@/components/player/PlayerChart'), {
     ssr: false,
   });
@@ -31,30 +31,37 @@ export default function page() {
       <div>
         <Banner title="투수" />
       </div>
-
-      <div className="flex flex-row justify-center h-screen flex-wrap bg-black/90 py-16">
-        <div className="flex flex-col h-auto mx-6">
-          {pitcherData.map((pitcher, index) => (
-            <PlayerCard key={index} player={pitcher} size="large" />
-          ))}
+      <div className="flex flex-col bg-black/90 h-screen">
+        <div className="flex flex-row justify-center flex-wrap py-16">
+          <div className="flex flex-col h-fit mx-6 items-center justify-self-center my-10">
+            {pitcherData.map((pitcher, index) => (
+              <PlayerCard key={index} player={pitcher} size="large" />
+            ))}
+          </div>
+          {/* AI 파트 */}
+          <div className="flex flex-col w-1/3 px-16 pl-22">
+            {/*그래프*/}
+            <div className="w-full">
+              <PlayerChart title={'선수 예측 데이터'} />
+            </div>
+            <div className="h-10 flex items-center justify-center">
+              <button className="w-1/2 h-full flex items-center justify-center font-bold text-white text-base bg-red-90 rounded-[30px]">
+                선수 성적 예측하기
+              </button>
+            </div>
+            {/* 한 줄 예측 */}
+            <div className="text-white pl-6 mt-3">AI 예측</div>
+            <div className="rounded-[5px] border-2 text-white border-white h-auto w-full mx-6 p-4">
+              누가 이겨? 내가 이겨~~ 루끼루끼 마 슈퍼루끼루끼루끼 마치마치 그
+              느낌적인 느낌느낌
+            </div>
+          </div>
         </div>
-        {/* AI 파트 */}
-        <div className="flex flex-col w-1/3 px-16 pl-22">
-          {/*그래프*/}
-          <div className="w-full">
-            <PlayerChart title={'선수 예측 데이터'} />
-          </div>
-          <div className="h-10 flex items-center justify-center">
-            <button className="w-1/2 h-full flex items-center justify-center font-bold text-white text-base bg-red-90 rounded-[30px]">
-              선수 성적 예측하기
-            </button>
-          </div>
-          {/* 한 줄 예측 */}
-          <div className="text-white pl-6 mt-3">AI 예측</div>
-          <div className="rounded-[5px] border-2 text-white border-white h-auto w-full mx-6 p-4">
-            누가 이겨? 내가 이겨~~ 루끼루끼 마 슈퍼루끼루끼루끼 마치마치 그
-            느낌적인 느낌느낌
-          </div>
+        <div className="flex text-white justify-center align-middle">
+          <button className="h-8 w-fit mx-2 flex flex-row">
+            <img src="/svgs/arrow-right.svg" className="w-8 h-8"></img>
+            <div className="text-lg">선수 기록 상세보기</div>
+          </button>
         </div>
       </div>
     </>
