fb86b1bbfe7588fc8a976622d403033b7a9a208d
SNXWXH <snxwxh@gmail.com>
2024-07-15 00:02:25 +0900

    💄 Design: 비교분석구간 datePicker 적용
    범위 설정 달력으로 변경
    백그라운드 및 선택된 날짜 색상 변경

--- src/app/(main)/ranking/daily/page.tsx
+++ src/app/(main)/ranking/daily/page.tsx
@@ -1,3 +1,4 @@
+import Calendar from '@/components/ranking/Calendar/Calendar';
 import dynamic from 'next/dynamic';
 
 export default function RankingDaily() {
@@ -10,15 +11,7 @@ export default function RankingDaily() {
       <div className="w-3/4 mx-auto text-white pb-16">
         <div className="h-8 mt-[51px] text-lg border-l-4 border-red-100 flex items-center">
           <p className="ml-3.5">비교 분석 구간:</p>
-          <input
-            type="date"
-            className="ml-3 w-32 h-8 text-sm text-center p-1 border border-white rounded-sm bg-inherit"
-          />
-          <p className="ml-3">~</p>
-          <input
-            type="date"
-            className="ml-3 w-32 h-8 text-sm text-center p-1 border border-white rounded-sm bg-inherit"
-          />
+          <Calendar />
         </div>
         <p className="my-4 ml-4 text-sm">* 기간 검색은 30일 이내</p>
         <Chart title="일자별 순위 그래프" />
