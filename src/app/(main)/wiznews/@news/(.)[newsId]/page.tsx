0737193ae1a1c1465dad5cb07a73dc1b3c4560e6
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-19 18:00:21 +0900

    ✨ Feat: 뉴스 id를 params prop에서 추출해 NewsContent에 전달하여 뉴스 상세 페이지 렌더링
    뉴스 id는 dynamic routes에 입력된 값임.

--- src/app/(main)/wiznews/@news/(.)[newsId]/page.tsx
+++ src/app/(main)/wiznews/@news/(.)[newsId]/page.tsx
@@ -3,9 +3,15 @@ import NewsContent from '@/components/wiznews/NewsContent';
 import { useRouter } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 
-export default function InterceptedWizNewsDetail() {
-  const ref = useRef<HTMLDivElement>(null);
+export type TParams = {
+  params: { newsId: string };
+};
+
+export default function InterceptedWizNewsDetail({ params }: TParams) {
+  const newsId = params.newsId;
   const router = useRouter();
+  const ref = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     const clickHandler = (e: MouseEvent) => {
       e.stopPropagation();
@@ -16,13 +22,14 @@ export default function InterceptedWizNewsDetail() {
     window.addEventListener('click', clickHandler);
     return () => window.removeEventListener('click', clickHandler);
   });
+
   return (
     <>
       <div
         className="w-full h-full bg-black bg-opacity-30 z-20 fixed top-0 flex justify-center items-center"
         ref={ref}
       >
-        <NewsContent />
+        <NewsContent newsId={newsId} />
       </div>
     </>
   );
