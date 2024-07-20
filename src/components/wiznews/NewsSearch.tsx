bd02da5a1c3460cc8d95208082fd58d23f5abbdf
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-20 23:28:25 +0900

    🎨 Style: 뉴스 데이터 fetch 제거, 해당 컴포넌트 제거 예정

--- src/components/wiznews/NewsSearch.tsx
+++ src/components/wiznews/NewsSearch.tsx
@@ -1,39 +1,6 @@
-'use client';
-
-import { create } from 'zustand';
 import Image from 'next/image';
-import { KeyboardEvent } from 'react';
-
-export type TNewsContent = {
-  artcContents: string;
-  artcTitle: string;
-  artcSeq: Number;
-  imgFilePath: string;
-  [key: string]: any;
-};
-
-export type TNewsList = TNewsContent[];
-
-type NewsListStore = {
-  newsList: TNewsList;
-  setNewsList: (news: TNewsList) => void;
-};
-
-export const useNewsListStore = create<NewsListStore>()((set) => ({
-  newsList: [],
-  setNewsList: (news) =>
-    set((state) => ({ newsList: [...state.newsList, ...news] })),
-}));
 
 export default function NewsSearch() {
-  const setNewsList = useNewsListStore((state) => state.setNewsList);
-  const handleEnter = async (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      // const inputElement = e.target as HTMLInputElement;
-      const newsData = await (await fetch(`/api/news`)).json();
-      setNewsList(newsData);
-    }
-  };
   return (
     <>
       <div className="flex justify-between w-[480px] max-sm:w-full my-7">
@@ -50,7 +17,6 @@ export default function NewsSearch() {
           id=""
           className="font-['DungGeunMo'] w-[436px] max-sm:w-[90%] h-8 px-3 rounded-[10px] shadow-[inset_2px_3px_4px_rgba(0,0,0,0.25)]"
           placeholder="원하는 뉴스를 검색해 보세요! 예) KT 위즈, 김민 선수"
-          onKeyDown={handleEnter}
         />
       </div>
     </>
