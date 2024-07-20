b7810faa16f1b7f28f20fa7b1a66c06789386e6f
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-20 23:25:30 +0900

    ✨ Feat: newsArea 컴포넌트에 뉴스 데이터 fetch 추가
    기존 NewsSearch에서 fetch 하던 것을 NewsArea에서 하도록 변경. 검색 창 제거 후 검색 선택 버튼 생성 예정.

--- src/components/wiznews/NewsArea.tsx
+++ src/components/wiznews/NewsArea.tsx
@@ -1,22 +1,57 @@
 'use client';
 
+import { create } from 'zustand';
 import NewsBalloon from './NewsBalloon';
-import { useNewsListStore } from './NewsSearch';
+import { useEffect } from 'react';
+
+export type TNewsContent = {
+  artcContents: string;
+  artcTitle: string;
+  artcSeq: Number;
+  imgFilePath: string;
+  [key: string]: any;
+} | null;
+
+export type TNewsList = TNewsContent[];
+
+type NewsListStore = {
+  newsList: TNewsList;
+  setNewsList: (news: TNewsList) => void;
+};
+
+export const useNewsListStore = create<NewsListStore>()((set) => ({
+  newsList: [],
+  setNewsList: (news) =>
+    set((state) => ({ newsList: [...state.newsList, ...news] })),
+}));
 
 export default function NewsArea() {
   const newsList = useNewsListStore((state) => state.newsList);
+  const setNewsList = useNewsListStore((state) => state.setNewsList);
+  useEffect(() => {
+    const fetchNews = async () => {
+      const newsData = await (
+        await fetch(`/api/news`, { cache: 'force-cache' })
+      ).json();
+      setNewsList(newsData);
+    };
+    fetchNews();
+  }, []);
   return (
     <>
       <div className="w-[820px] max-lg:w-[500px] h-[600px] overflow-y-scroll no-scrollbar">
         {newsList.length &&
-          newsList.map((news, index) => (
-            <NewsBalloon
-              key={news.artcTitle}
-              direction={index % 2 === 0 ? 'right' : 'left'}
-              content={news.artcTitle}
-              id={news.artcSeq}
-            />
-          ))}
+          newsList.map(
+            (news, index) =>
+              news && (
+                <NewsBalloon
+                  key={news.artcTitle}
+                  direction={index % 2 === 0 ? 'right' : 'left'}
+                  content={news.artcTitle}
+                  id={news.artcSeq}
+                />
+              ),
+          )}
       </div>
     </>
   );
