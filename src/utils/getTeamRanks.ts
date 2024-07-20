fe73fa8ce88c0a6c1845ba33b4f8de375936c469
SNXWXH <snxwxh@gmail.com>
2024-07-20 13:25:40 +0900

    ✨ Feat: 선택 팀에 따라 애니메이션 적용 중

--- src/utils/getTeamRanks.ts
+++ src/utils/getTeamRanks.ts
@@ -4,11 +4,21 @@ export const getTeamRanks = (teamName: string) => {
   const yearRankJson = year_rank;
 
   const yearRank: any[] = [];
+  const year: any[] = [];
 
+  // yearRankJson.forEach((item) => {
+  //   const teamData = item.data.find((v) => v.team === teamName);
+  //   yearRank.push(teamData ? teamData.rank : 'null');
+  // });
   yearRankJson.forEach((item) => {
     const teamData = item.data.find((v) => v.team === teamName);
-    yearRank.push(teamData ? teamData.rank : 'null');
+    if (teamData) {
+      yearRank.push(teamData.rank);
+      year.push(item.year);
+    }
   });
+  // yearRank.push(teamData ? teamData.rank : 'null');
 
-  return yearRank;
+  return { yearRank, year };
+  // return yearRank;
 };
