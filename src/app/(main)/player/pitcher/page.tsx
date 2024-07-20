4332b3ba44215632d393fb8ca9102a394fd591b9
mj <mjk4534@ewhain.net>
2024-07-20 20:44:20 +0900

    ✨ Feat: 카드 클릭 시 해당 선수 백넘버 세부페이지로 라우팅

--- src/app/(main)/player/pitcher/page.tsx
+++ src/app/(main)/player/pitcher/page.tsx
@@ -1,18 +1,28 @@
 'use client';
 
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import CardFront from '@/components/tradingCard/CardFront';
 import pitcher_data from '#/data/playerFront/pitcher_data.json';
 
 const pitcherData = pitcher_data.data.list;
 
-export default function Pitcher() {
+export default function PitcherMain() {
+  const router = useRouter();
+  const handleCardClick = (backNum: string) => {
+    router.push(`/player/pitcher/${backNum}`);
+  };
   return (
     <>
       <div className="bg-white flex justify-center items-center h-min flex-wrap ">
         <div className="flex flex-wrap flex-row gap-6 p-6 justify-center items-center object-center w-3/4">
           {pitcherData.map((pitcher, index) => (
-            <CardFront key={index} player={pitcher} size="medium" />
+            <CardFront
+              key={index}
+              player={pitcher}
+              size="medium"
+              onClick={() => handleCardClick(pitcher.backNum)}
+            />
           ))}
         </div>
       </div>
