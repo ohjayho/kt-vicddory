0f63f5204b3b706e3cf9712c3bd5ce002842b12e
yheseul <kangyheseul@naver.com>
2024-07-22 02:23:09 +0900

    ✨ Feat: 연동된 결과 api 뿌림

--- src/components/test/result/CaptureArea.tsx
+++ src/components/test/result/CaptureArea.tsx
@@ -1,33 +1,51 @@
-import { forwardRef, RefObject } from 'react';
+'use client'
+
+import { useEffect, useState, forwardRef, RefObject } from 'react';
 import Image from 'next/image';
+import Loading from '@/app/(test)/test/loading';
 
 type TCaptureAreaProps = {
   divRef: RefObject<HTMLDivElement>;
 };
 
-const CaptureArea = forwardRef<HTMLDivElement, TCaptureAreaProps>(
-  ({ divRef }) => {
-    return (
-      <>
-        <div
-          ref={divRef}
-          className="w-full h-[600px] bg-[#FFFFFF] flex flex-col justify-center items-center relative"
-        >
-          <div className="absolute flex justify-center items-center top-10 w-72 h-9 bg-red-100 rounded-full text-white">
-            당신의 ♥천생연분♥ 야구선수는?
-          </div>
-          <Image
-            src="/svgs/test/result/resultBackground.svg"
-            alt="emblem"
-            width={500}
-            height={120}
-            className="absolute bottom-[-1px] w-full"
-          />
-        </div>
-      </>
-    );
-  },
-);
+const CaptureArea = forwardRef<HTMLDivElement, TCaptureAreaProps>(({ divRef }, ref) => {
+  const [result, setResult] = useState<{ position: string; response: string } | null>(null);
+
+  useEffect(() => {
+    const storedResult = sessionStorage.getItem('testResult');
+    if (storedResult) {
+      const parsedResult = JSON.parse(storedResult);
+      setResult(parsedResult);
+    }
+  }, []);
+
+  if (!result) {
+    return <Loading />;
+  }
+
+  return (
+    <div
+      ref={divRef}
+      className="w-full h-[600px] bg-[#FFFFFF] flex flex-col justify-center items-center relative"
+    >
+      <div className="absolute flex justify-center items-center top-10 w-72 h-9 bg-red-100 rounded-full text-white">
+        당신의 ♥천생연분♥ 야구선수는?
+      </div>
+      <div className="result-container">
+        <h1>테스트 결과</h1>
+        <h2>포지션: {result.position}</h2>
+        <p>{result.response}</p>
+      </div>
+      <Image
+        src="/svgs/test/result/resultBackground.svg"
+        alt="emblem"
+        width={500}
+        height={120}
+        className="absolute bottom-[-1px] w-full"
+      />
+    </div>
+  );
+});
 
 CaptureArea.displayName = "CaptureArea";
 
