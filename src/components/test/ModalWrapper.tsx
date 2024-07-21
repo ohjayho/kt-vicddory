1bd7be5c569cc16390eda4cf30478e08b6c5d74f
yheseul <kangyheseul@naver.com>
2024-07-22 02:23:59 +0900

    💄 Design: 모달 뒷 배경 투명도 수정

--- src/components/test/ModalWrapper.tsx
+++ src/components/test/ModalWrapper.tsx
@@ -13,7 +13,7 @@ export default function ModalWrapper ({ children }: { children: React.ReactNode 
 
   return (
     <>
-      <div onClick={closeModal} className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
+      <div onClick={closeModal} className="fixed inset-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
       {React.cloneElement(children as React.ReactElement, { closeModal })}
     </div>
     </>
