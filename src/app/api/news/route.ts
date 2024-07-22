59a6b809c74be77cf1925b30858436a88149acef
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-22 13:32:52 +0900

    ✨ Feat: 뉴스 API 데이터 개수, 페이지 params 추가

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,11 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const searchMax = searchParams.get('searchMax');
+  const pageNum = searchParams.get('pageNum');
+  console.log(searchMax, pageNum, 'params test');
   try {
-    const response = await fetch(`${process.env.API_URL}/news_list`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    const response = await fetch(
+      `${process.env.API_URL}/news_list?searchmax=${searchMax}&page=${pageNum}`,
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
     const news = await response.json();
     return NextResponse.json(news.data.list, { status: 200 });
   } catch (error) {
