e4a956e3c74f5d6f85cfa75d1c744c032b7e7748
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-23 02:10:51 +0900

    ✨ Feat: url로 직접 접근 시 상세 페이지 fetch 기능 추가, 동적 imports로 tts 로딩 기능 추가

--- src/components/wiznews/NewsContent.tsx
+++ src/components/wiznews/NewsContent.tsx
@@ -2,57 +2,59 @@
 import { useRouter } from 'next/navigation';
 import { MouseEventHandler, useEffect, useState } from 'react';
 import Image from 'next/image';
-import NewsTTS from './NewsTTS';
 import { TNewsContent, useNewsListStore } from './NewsArea';
+import dynamic from 'next/dynamic';
 
 type TNewsId = {
   newsId: string;
 };
 
 export default function NewsContent({ newsId }: TNewsId) {
-  const [theNews, setTheNews] = useState<TNewsContent>(null);
+  const [newsData, setNewsData] = useState<TNewsContent | undefined>(null);
+  const [theNews, setTheNews] = useState<TNewsContent | undefined>(null);
   const newsList = useNewsListStore((state) => state.newsList);
-  const setNewsList = useNewsListStore((state) => state.setNewsList);
-  const fetchNews = async () => {
-    //NewsSearch에서 caching 된 경우 재호출되지 않음.
-    //하지만 뉴스의 URL을 공유했을 경우 Zustand의 store에 뉴스 정보가 없기 때문에 fetch해야함.
-    const newsData = await (
-      await fetch(`/api/news`, { cache: 'force-cache' })
-    ).json();
-    setNewsList(newsData);
-  };
-
   const router = useRouter();
   const handleBacklink: MouseEventHandler<HTMLButtonElement> = () => {
     router.back();
   };
 
-  useEffect(() => {
-    //조건문 할 필요없이 force-cache 옵션으로 대체
-    // if (newsList.length === 0) {
-    ///뉴스가 없다면
-    // console.log('뉴스가 없대');
-    fetchNews();
-    // }
-  }, []);
+  const fetchDetailedNews = async () => {
+    const detailedNews = await (
+      await fetch(`/api/detailednews?params=${newsId}`)
+    ).json();
+    console.log(detailedNews, '디테일');
+    return detailedNews;
+  };
+
+  const DynamicTTS = dynamic(() => import('./NewsTTS'), {
+    loading: () => <div>Loading TTS...</div>,
+    ssr: false,
+  });
 
   useEffect(() => {
-    const theNews = newsList.find(
-      // news is possibly null, so added news &&
-      (news) => news && news.artcSeq === parseInt(newsId),
-    );
-
-    if (theNews) {
-      //undefiend 일 수도 있으니 타입 체크. 타입오류 해결 용도
-      setTheNews(theNews);
+    if (newsList.length) {
+      setNewsData(
+        newsList.find(
+          // news is possibly null, so added news &&
+          (news) => news && news.artcSeq === parseInt(newsId),
+        ),
+      );
+    } else {
+      const getDetailedNews = async () => {
+        setNewsData(await fetchDetailedNews());
+      };
+      getDetailedNews();
     }
-    console.log(theNews, '더뉴스');
   }, [newsId, newsList]);
+
+  useEffect(() => {
+    setTheNews(newsData);
+  }, [newsData]);
 
   return (
     <>
       {theNews && (
-        <section className="w-[820px] max-lg:w-[80%] h-[55%] bg-[#0a0a0e] rounded-[5px] p-5 font-['DungGeunMo'] text-white">
+        <section className="w-[820px] max-lg:w-[80%] h-[63%] bg-[#0a0a0e] rounded-[5px] p-5 font-['DungGeunMo'] text-white">
           <div className="h-full shadow-[0_0_30px_rgba(237,136,136,0.84)] bg-[url('/images/wiznews/newsPattern.png')] flex flex-col 내부컨테이너">
             <div className="h-14 bg-[rgba(0,0,0,0.44)] p-8 버튼 칸">
               <button
@@ -62,22 +64,28 @@ export default function NewsContent({ newsId }: TNewsId) {
                 ← 목록
               </button>
             </div>
-            <div className="flex max-md:flex-col justify-between pt-8 px-8 pb-5 제목 칸">
-              <h1 className="w-[80%] max-md:w-full 제목">
-                {theNews.artcTitle}
-              </h1>
-              <h3 className="flex justify-end max-md:mt-4 날짜">2024-01-17</h3>
+            <div className="flex flex-col items-center">
+              <div className="w-full flex max-md:flex-col justify-between pt-8 px-8 pb-5 제목 칸">
+                <h1 className="w-[80%] max-md:w-full 제목">
+                  {theNews.artcTitle}
+                </h1>
+                <h3 className="flex justify-end max-md:mt-1 날짜">
+                  2024-01-17
+                </h3>
+              </div>
+              <DynamicTTS text={theNews.artcContents} />
             </div>
             <div className="px-8 pb-8 overflow-scroll no-scrollbar 본문 칸">
-              <NewsTTS text={theNews.artcContents} />
-              <Image
-                src={theNews.imgFilePath}
-                width={500}
-                height={0}
-                alt="news_image"
-                className="w-full"
-              />
-              <p className="h-full overflow-y-scroll no-scrollbar">
+              {theNews.imgFilePath && (
+                <Image
+                  src={theNews.imgFilePath}
+                  width={500}
+                  height={0}
+                  alt="news_image"
+                  className="w-full mt-4"
+                />
+              )}
+              <p className="h-full overflow-y-scroll no-scrollbar mt-4">
                 {theNews.artcContents}
               </p>
             </div>
