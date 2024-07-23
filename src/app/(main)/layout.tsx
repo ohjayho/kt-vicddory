b215cd7dcbb1e78abe9394b3f039bd2b67b8fef4
yheseul <kangyheseul@naver.com>
2024-07-23 12:52:59 +0900

    🔥 WIP: 테스트 모달 구현 진행 중

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,15 +4,18 @@ import ModalWrapper from '@/components/test/ModalWrapper';
 export default function MainLayout({
   children,
   modal,
+  questions,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
+  questions: React.ReactNode;
 }) {
   return (
     <>
       <Header />
       {children}
       {modal && <ModalWrapper>{modal}</ModalWrapper>}
+      {questions}
     </>
   );
 }
