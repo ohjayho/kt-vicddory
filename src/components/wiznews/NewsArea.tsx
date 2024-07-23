f9cad3086196c0a83b907db3653486316aaa5122
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-24 01:01:45 +0900

    💄 Design: ai 뉴스 말풍선 badge 위한 패딩 추가

--- src/components/wiznews/NewsArea.tsx
+++ src/components/wiznews/NewsArea.tsx
@@ -57,7 +57,7 @@ export default function NewsArea() {
   }, []);
   return (
     <>
-      <div className="w-[820px] max-lg:w-[500px] h-[600px] overflow-y-scroll no-scrollbar">
+      <div className="w-[820px] max-lg:w-[500px] h-[600px] overflow-y-scroll no-scrollbar pt-1">
         {newsList.length &&
           newsList.map(
             (news, index) =>
