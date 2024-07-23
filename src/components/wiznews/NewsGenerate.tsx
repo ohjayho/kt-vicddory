0ca2aa8e41cebfda48d8638fdc84d9eab1a57658
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-24 01:03:46 +0900

    💄 Design: ai 뉴스 생성 버튼과 로딩 상태 디자인 변경

--- src/components/wiznews/NewsGenerate.tsx
+++ src/components/wiznews/NewsGenerate.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import { useState } from 'react';
 import { useNewsListStore } from './NewsArea';
 export default function NewsGenerate() {
-  // const [dateType, setDateType] = useState('text');
   const [date, setDate] = useState('2024-07-11');
   const [loading, setLoading] = useState(false);
   const setNewsList = useNewsListStore((state) => state.setNewsList);
@@ -41,17 +40,20 @@ export default function NewsGenerate() {
           id=""
           min="2024-07-01"
           max="2024-07-22"
-          className="w-[40%] font-['DungGeunMo'] max-sm:w-[90%] h-full px-3 rounded-[10px] shadow-[inset_2px_3px_4px_rgba(0,0,0,0.25)]"
+          className="w-[40%] font-['DungGeunMo'] h-full px-3 rounded-[10px] shadow-[inset_2px_3px_4px_rgba(0,0,0,0.25)]"
           value={date}
-          // onFocus={() => setDateType('date')}
           onChange={(e) => setDate(e.target.value)}
         />
         <button
-          className="h-full w-[160px] flex justify-center items-center font-['DungGeunMo'] text-white max-sm:w-[90%] px-3 rounded-[10px] shadow-[inset_2px_3px_4px_rgba(0,0,0,0.25)]"
+          className="generate-btn-wrapper h-full w-[140px] text-white font-['DungGeunMo']"
           onClick={fetchAiNews}
         >
-          <div className="h-full w-full text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-base px-5 py-2.5 flex justify-center items-center">
-            {loading ? '로딩 중' : 'AI 뉴스 생성'}
+          <div className="generate-btn-content flex items-center justify-center text-base">
+            {loading ? (
+              <div className="ball animate-spin"></div>
+            ) : (
+              'AI 뉴스 생성'
+            )}
           </div>
         </button>
       </div>
