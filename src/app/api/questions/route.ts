6f5ee7b36dc9321ccabc994ba7f18c8673258c42
SNXWXH <snxwxh@gmail.com>
2024-07-27 22:40:25 +0900

    🐛 Fix: build 오류 해결

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { TQuestionHandlerProps } from '@/types';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const response = await fetch(`${process.env.API_URL}/generate_questions`, {
       method: 'POST',
