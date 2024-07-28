f611a714cbea6caf168a9eb682366c3a1fe17cff
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-28 15:45:02 +0900

    🎨 Style: 사용되지 않는 import 부분 삭제

--- src/components/mobile/MobileMenuBtn.tsx
+++ src/components/mobile/MobileMenuBtn.tsx
@@ -3,8 +3,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
-import { FaChevronDown } from 'react-icons/fa';
-import { FaChevronUp } from 'react-icons/fa';
 
 type MobileMenuBtnType = {
   title: string;
