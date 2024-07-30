a98656e318f348574ed7af7dafb7b58385ea5215
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-30 10:39:48 +0900

    ✨ Feat: 메타태그 제목과 설명 추가

--- src/app/(main)/player/batter/layout.tsx
+++ src/app/(main)/player/batter/layout.tsx
@@ -1,5 +1,11 @@
 import Banner from '@/components/player/Banner';
 
+export const metadata = {
+  title: '타자',
+  description:
+    'kt wiz에서 인터랙티브한 선수 카드와 상세 페이지를 확인하세요. 현재 선수의 경기력과 AI가 예측한 미래 성과를 멋진 애니메이션과 함께 제공합니다.',
+};
+
 export default function BatterLayout({
   children,
 }: {
