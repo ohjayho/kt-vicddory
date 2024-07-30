a17b6a3976a2ab93c64abeebb933135bad1382f5
Jay Ho Oh <ohjayhoi@gmail.com>
2024-07-30 14:19:13 +0900

    🎨 Style: push용 마침표 삭제

--- src/app/(main)/wiznews/layout.tsx
+++ src/app/(main)/wiznews/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 export const metadata = {
   title: '위즈 뉴스',
   description:
-    'AI가 경기 일정에 따라 실감나는 뉴스를 자동으로 생성하는 kt wiz의 새로운 기능! 최신 경기 업데이트와 팀 소식을 놓치지 마세요.',
+    'AI가 경기 일정에 따라 실감나는 뉴스를 자동으로 생성하는 kt wiz의 새로운 기능! 최신 경기 업데이트와 팀 소식을 놓치지 마세요',
 };
 
 export default function NewsLayout({
